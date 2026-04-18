e347da0767c8882e4642a039089da758d99f7704
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-18 18:41:06 +0900

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -6,8 +6,8 @@ package «asr_unified_proof» {
 
 @[default_target]
 lean_lib «ASR» {
-  -- ASR, Conjectures, Constants フォルダをすべて含める設定
-  globs := #[.submodules `ASR, .submodules `Conjectures, .submodules `Constants]
+  -- 全てのサブモジュールを自動的に認識させる設定
+  globs := #[.andSubmodules `ASR, .andSubmodules `Conjectures, .andSubmodules `Constants]
 }
 
 require mathlib from git
